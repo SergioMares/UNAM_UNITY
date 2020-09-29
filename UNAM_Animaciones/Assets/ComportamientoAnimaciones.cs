4d57581a0d6c4218132fcaff4178935dde6ec5b1
Sergio Mares Martínez <sergiomaresmtz@gmail.com>
2020-09-28 20:45:14 -0500

    Animaciones en Unity

---
+++ UNAM_Animaciones/Assets/ComportamientoAnimaciones.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComportamientoAnimaciones : MonoBehaviour
+{
+    private Animation maniqui;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Asegurarse de que el GameObject donde se aloje este Script, tenga el 
+        //componente animation para poder ejecutarse correctamente
+        maniqui = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            //el método play tiene como parámetro el nombre de la animación.
+            maniqui.Play("BasicMotions@Jump01 [RootMotion]");
+        }
+    }
+}
---
+++ UNAM_Animaciones/Assets/EventosAnim.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventosAnim : MonoBehaviour
+{
+
+    public void evento()
+    {
+        Debug.Log("se mandó llamar el evento");
+    }
+}
