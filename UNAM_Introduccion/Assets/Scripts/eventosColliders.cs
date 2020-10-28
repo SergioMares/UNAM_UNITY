319101e08bedf9f97e515fccfb24396cdb07ef3a
Sergio Mares Martínez <sergiomaresmtz@gmail.com>
2020-10-28 15:00:35 -0600

    Ahora con Físicas avanzadas

---
+++ UNAM_Introduccion/Assets/Scripts/eventosColliders.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class eventosColliders : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("entró la colisión");
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Se mantiene en la colisión");
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Salió de la colisión");
+    }
+}
---
+++ UNAM_Introduccion/Assets/Scripts/eventosTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class eventosTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Trigger entra en contacto");
+    }
+
+    void OnTriggerStay(Collider collision)
+    {
+        Debug.Log("Trigger en contacto");
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        Debug.Log("Trigger fuera de contacto");
+    }
+}
+
+
